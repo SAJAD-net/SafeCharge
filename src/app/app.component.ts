38142f1ade1ca5079ee060dd3a80bb01e441ff39
SAJADnet <sajadsajad.sch@gmail.com>
2023-07-22 23:04:09 +0430

    Enabling the background mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { BackgroundMode } from '@ionic-native/background-mode/ngx';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
 export class AppComponent {
-  constructor() {}
-}
+  constructor(
+    private platform: Platform,
+    private backgroundMode: BackgroundMode
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.backgroundMode.enable();
+    });
+  }
+} 
